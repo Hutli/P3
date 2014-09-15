fde7f829723f59bb42aef0e6c07c8e1d0c4f121a
cnHeider <mrnaah@gmail.com>
2014-09-15 22:03:25 +0200

    Fixed allocation thrown exception but now type initialiser exception instead

--- Source-2014-09-15/WebRadio/Program.cs
+++ Source-2014-09-15/WebRadio/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 
 using libspotifydotnet;
+using System.Runtime.InteropServices;
 
 namespace WebRadio
 {
@@ -15,28 +16,45 @@ namespace WebRadio
 
         static void Main(string[] args)
         {
-            IntPtr session_callback = new IntPtr();
-            /*session_callback.logged_in = &logged_in;
-             session_callback.notify_main_thread = &notify_main_thread;
-             session_callback.music_delivery = &music_delivery;
-             session_callback.metadata_updated = &metadata_updated;
-             session_callback.play_token_lost = &play_token_lost;
-             session_callback.log_message = NULL;
-             session_callback.end_of_track = &end_of_track;
-             */
+            libspotify.sp_session_callbacks callbacks = new libspotify.sp_session_callbacks();
+            /*callbacks.connection_error = Marshal.GetFunctionPointerForDelegate(fn_connection_error_delegate);
+            callbacks.end_of_track = Marshal.GetFunctionPointerForDelegate(fn_end_of_track_delegate);
+            callbacks.get_audio_buffer_stats = Marshal.GetFunctionPointerForDelegate(fn_get_audio_buffer_stats_delegate);
+            callbacks.log_message = Marshal.GetFunctionPointerForDelegate(fn_log_message);
+            callbacks.logged_in = Marshal.GetFunctionPointerForDelegate(fn_logged_in_delegate);
+            callbacks.logged_out = Marshal.GetFunctionPointerForDelegate(fn_logged_out_delegate);
+            callbacks.message_to_user = Marshal.GetFunctionPointerForDelegate(fn_message_to_user_delegate);
+            callbacks.metadata_updated = Marshal.GetFunctionPointerForDelegate(fn_metadata_updated_delegate);
+            callbacks.music_delivery = Marshal.GetFunctionPointerForDelegate(fn_music_delivery_delegate);
+            callbacks.notify_main_thread = Marshal.GetFunctionPointerForDelegate(fn_notify_main_thread_delegate);
+            callbacks.offline_status_updated = Marshal.GetFunctionPointerForDelegate(fn_offline_status_updated_delegate);
+            callbacks.play_token_lost = Marshal.GetFunctionPointerForDelegate(fn_play_token_lost_delegate);
+            callbacks.start_playback = Marshal.GetFunctionPointerForDelegate(fn_start_playback);
+            callbacks.stop_playback = Marshal.GetFunctionPointerForDelegate(fn_stop_playback);
+            callbacks.streaming_error = Marshal.GetFunctionPointerForDelegate(fn_streaming_error_delegate);
+            callbacks.userinfo_updated = Marshal.GetFunctionPointerForDelegate(fn_userinfo_updated_delegate);*/
+
+            IntPtr callbacksPtr = Marshal.AllocHGlobal(Marshal.SizeOf(callbacks));
+            Marshal.StructureToPtr(callbacks, callbacksPtr, true);
 
             libspotify.sp_session_config config = new libspotify.sp_session_config();
             config.api_version = libspotify.SPOTIFY_API_VERSION;
             config.user_agent = "WebRadio";
             config.application_key_size = sizeof(Int32);
-            config.application_key = (IntPtr)appkey;
+            config.application_key = Marshal.AllocHGlobal(appkey.Length);
             config.cache_location = "tmp";
             config.settings_location = "tmp";
-            config.callbacks = session_callback;
+            config.callbacks = callbacksPtr;
 
-            IntPtr sessionPtr = new IntPtr();
+            Marshal.Copy(appkey, 0, config.application_key, appkey.Length);
 
-            libspotify.sp_session_create(ref config, out sessionPtr);
+            IntPtr sessionPtr;
+            libspotify.sp_error err = libspotify.sp_session_create(ref config, out sessionPtr);
+
+            if (err == libspotify.sp_error.OK)
+            {
+                libspotify.sp_session_set_connection_type(sessionPtr, libspotify.sp_connection_type.SP_CONNECTION_TYPE_WIRED);
+            }
 
             Console.ReadKey();
         }
