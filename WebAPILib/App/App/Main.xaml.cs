cbe234f5d52c78df2e1919105df07baccbff0eef
zanderdk <moonfrogdk@gmail.com>
2014-09-24 23:05:29 +0200

    created app using forms

---
+++ WebAPILib/App/App/Main.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App
+{	
+	public partial class Main : ContentPage
+	{	
+		public Main ()
+		{
+			InitializeComponent ();
+
+		}
+
+		public void onSearch(object sender, EventArgs args)
+		{
+			Entry et = this.FindByName<Entry> ("txtSearch");
+			((Button)sender).Text = et.Text;
+
+		}
+	}
+}
+
---
+++ WebAPILib/App/App/Resault.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App
+{	
+	public partial class Resault : ContentPage
+	{	
+		public Resault ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
