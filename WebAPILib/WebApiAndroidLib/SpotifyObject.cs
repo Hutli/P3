d9798e97101bc804a3a807424f3e8fb766db942a
zanderdk <moonfrogdk@gmail.com>
2014-09-24 08:27:13 +0200

    android app

---
+++ WebAPILib/WebApiAndroidLib/SpotifyObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WebAPILib {
+	public abstract class SpotifyObject {
+		protected string _id;
+		protected string _name;
+
+		public SpotifyObject (string id, string name) {
+			_id = id;
+			_name = name;
+		}
+
+		public search SearchResult { get; set; }
+
+		public string ID { get { return _id; } }
+
+		public string Name { get { return _name; } }
+
+		public virtual string URI { get { return ""; } }
+		//TODO generate URI
+	}
+}
+
