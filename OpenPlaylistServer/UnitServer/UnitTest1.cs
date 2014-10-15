fa0087b7aed05b79ad2b63d94397495d238c5746
cnHeider <mrnaah@gmail.com>
2014-10-15 15:48:50 +0200

    Testing branch

---
+++ OpenPlaylistServer/UnitServer/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitServer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
