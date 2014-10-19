2ea201b7f3116488156f9dc3f803d9e26c3865c9
KaFuCh <kfch13@student.aau.dk>
2014-10-19 23:27:05 +0200

    noget playlist test fix, ikke done
    track har ikke nogen constructor wtf

--- OpenPlaylistServer/TestSuite/TestPlaylist.cs
+++ OpenPlaylistServer/TestSuite/TestPlaylist.cs
@@ -5,49 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using OpenPlaylistServer;
+using SpotifyDotNet;
 
-namespace TestSuite
-{
-    public class TestPlaylist
-    {
+namespace TestSuite {
+    public class TestPlaylist {
 
         [Fact]
-        public void TestNextTrack()
-        {
+        public void NextTrackHasHighestVotes() { //Tests that the next track is the one with highest votes
             List<User> users = new List<User>();
             Playlist pl = new Playlist();
-
             PTrack pTrack1 = new PTrack();
             PTrack pTrack2 = new PTrack();
-
-            User a = new User();
-            User b = new User();
-            User c = new User();
-            a.Vote = pTrack1;
-            b.Vote = pTrack2;
-            c.Vote = pTrack1;
-            users.Add(a);
-            users.Add(b);
-            users.Add(c);
+            User a = new User("1234");
+            User b = new User("2345");
+            User c = new User("3456");
 
             pl._tracks.Add(pTrack1);
             pl._tracks.Add(pTrack2);
 
-            Assert.Equal(pTrack1, pl.NextTrack(users));
+            a.Vote = pTrack2;
+            b.Vote = pTrack1;
+            c.Vote = pTrack1;
+
+            users.Add(a); //votes for track 2
+            Assert.Equal(pTrack2, pl.NextTrack(users)); //t1 has 0 votes, t2 has 1 vote, tests that t2 is next
+
+            users.Add(b); //votes for track 1
+            users.Add(c); //votes for track 1
+            Assert.Equal(pTrack1, pl.NextTrack(users)); //t1 has 2 votes, t2 has 1 vote, tests that t1 is next
         }
 
         [Fact]
-        public void TestCurrentStanding(){
+        public void CurrentStandingGivesCorrectRes() {
             List<User> users = new List<User>();
             Playlist pl = new Playlist();
-
             PTrack pTrack1 = new PTrack();
-
-            User a = new User();
+            User a = new User("1234");
 
             users.Add(a);
             a.Vote = pTrack1;
-
             pl._tracks.Add(pTrack1);
 
             Assert.Equal(0, pTrack1.TScore);
@@ -56,5 +52,23 @@ namespace TestSuite
 
             Assert.Equal(1, pTrack1.TScore);
         }
+
+        [Fact]
+        public void AddByURIAddsTrack() {
+            Playlist pl = new Playlist();
+            Assert.False(pl._tracks.Exists(e => e.Name == "Obliteration of the Weak"));
+            pl.AddByURI("19pTAbMZmWsgGkYZ4v2TM1");
+            Assert.True(pl._tracks.Exists(e => e.Name == "Obliteration of the Weak"));
+        }
+
+        //[Fact]
+        //public void AddByRefAddsTrack() {   PROBLEM! Track har ingen constructor
+        //    Playlist pl = new Playlist();
+        //    Track t = new Track();
+        //    PTrack pt = new PTrack(t);
+        //    Assert.False(pl._tracks.Contains(pt));
+        //    pl.AddByRef(t);
+        //    Assert.Single(pl._tracks, pt);
+        //} 
     }
 }
