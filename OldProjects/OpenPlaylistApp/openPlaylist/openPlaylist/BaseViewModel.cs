8a3a474616bc38f4e050fa1852aa0c40d7c576cc
cnHeider <mrnaah@gmail.com>
2014-10-22 09:35:26 +0200

    move again

---
+++ OldProjects/OpenPlaylistApp/openPlaylist/openPlaylist/BaseViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace openPlaylist
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public BaseViewModel()
+        {
+
+        }
+
+        private bool isBusy;
+        public bool IsBusy { get { return isBusy; } set { SetProperty(ref isBusy, value, "IsBusy"); } }
+
+        protected void SetProperty<T>(
+            ref T backingStore, T value,
+            string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return;
+
+            backingStore = value;
+
+            OnPropertyChanged(propertyName);
+        }
+
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
