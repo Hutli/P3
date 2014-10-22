fe4cfc7a2c9688ef54ea63b68af5a9d3ade1784f
cnHeider <mrnaah@gmail.com>
2014-10-22 18:01:54 +0200

    Splittet Playlist up into several files

---
+++ OpenPlaylistServer/OpenPlaylistServer/PTrack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpotifyDotNet;
+
+namespace OpenPlaylistServer {
+    public class PTrack {
+        private int _pScore = 0;
+        private string _name;
+        private int _duration;
+        private Track _track;
+        private int _tScore = 0;
+
+        public int TScore {
+            get {
+                return _tScore;
+            }
+            set {
+                _tScore = value;
+            }
+        }
+
+        public void UpdatePScore(List<User> users) {
+            _pScore += users.Count;
+        }
+
+        public void ResetPScore() {
+            _pScore = 0;
+        }
+
+        public int PScore {
+            get {
+                return _pScore;
+            }
+        }
+
+        public string Name {
+            get {
+                return _name;
+            }
+        }
+
+        public int Duration {
+            get {
+                return _duration; //Maybe to seconds idk??
+            }
+        }
+
+        public Track Track {
+            get {
+                return _track;
+            }
+        }
+
+        public PTrack() {
+        }
+
+        public PTrack(Track track) {
+            _track = track;
+            _name = track.Name;
+            _duration = track.Duration;
+        }
+
+        public override string ToString() {
+            return _name;
+        }
+    }
+}
---
+++ OpenPlaylistServer/OpenPlaylistServer/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPlaylistServer {
+    public class User {
+        private string _id;
+        private PTrack _vote;
+
+        public string Id {
+            get {
+                return _id;
+            }
+        }
+
+        public PTrack Vote {
+            get {
+                return _vote;
+            }
+            set {
+                _vote = value;
+            }
+        }
+
+        public User(string id) {
+            _id = id;
+        }
+
+        public User(string id, PTrack vote) {
+            _id = id;
+            _vote = vote;
+        }
+    }
+}
