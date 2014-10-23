185532c2596ee4436efe527bc05511da01f580ac
cnHeider <mrnaah@gmail.com>
2014-10-23 13:52:18 +0200

    System defined

---
+++ Report/clean_folders.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+find . -type f -name '*.log' -delete
+find . -type f -name '*.aux' -delete
+find . -type f -name '*.dvi' -delete
+find . -type f -name '*.lof' -delete
+find . -type f -name '*.lot' -delete
+find . -type f -name '*.bit' -delete
+find . -type f -name '*.idx' -delete
+find . -type f -name '*.glo' -delete
+find . -type f -name '*.bbl' -delete
+find . -type f -name '*.ilg' -delete
+find . -type f -name '*.toc' -delete
+find . -type f -name '*.ind' -delete
+find . -type f -name '*.out' -delete
+find . -type f -name '*.synctex.gz' -delete
+find . -type f -name '*.blg' -delete
+find . -type f -name '*.thm' -delete
+find . -type f -name '*.pre' -delete
+find . -type f -name '*.bak' -delete
+find . -type f -name '*.mine' -delete
+find . -type f -name '*.r92' -delete
+find . -type f -name '*.r93' -delete
+find . -type f -name '*.xml' -delete
+find . -type f -name '*main-blx.*' -delete
+find . -type f -name '*.bcf' -delete
+find . -type f -name '*.lox' -delete
