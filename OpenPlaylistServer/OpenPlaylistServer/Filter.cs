1debf06396c19b7888ea2e79923db6961f4bbb2c
KaFuCh <kfch13@student.aau.dk>
2014-11-04 09:26:38 +0100

    filter (not done)

---
+++ OpenPlaylistServer/OpenPlaylistServer/Filter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPlaylistServer {
+    public class Filter {
+        private List<FilterItem> Filter = new List<FilterItem>();
+        public void AddFilter(FilterItem filter) {
+            Filter.Add(filter);
+        }
+        public void AddFilter(Object attribtute, Object limit, Boolean white) {
+            FilterItem tmp = new FilterItem(attribtute, limit, white);
+            Filter.Add(tmp);
+        }
+
+        public void ApplyFilter(List<PlaylistTrack> tracks){
+            
+        }
+    }
+}
---
+++ OpenPlaylistServer/OpenPlaylistServer/FilterItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPlaylistServer {
+    public class FilterItem {
+        Object Attribute = new Object();
+        Object Limit = new Object();
+        Boolean White = new Boolean();
+        public FilterItem(Object attribute, Object limit, Boolean white) {
+            Attribute = attribute;
+            Limit = limit;
+            White = white;
+        }
+    }
+}
