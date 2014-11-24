6cbfc7b61831d30212eb923e84ae00f0889cea77
KaFuCh <kfch13@student.aau.dk>
2014-11-24 16:52:14 +0100

    magi, nu kan du få playlist

---
+++ OpenPlaylistServer/OpenPlaylistServer/PlaylistModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+using Newtonsoft.Json;
+
+namespace OpenPlaylistServer
+{
+    public class PlaylistModule : NancyModule
+    {
+        public PlaylistModule(IPlaylistService playlistService)
+        {
+            Get["playlist"] = e => JsonConvert.SerializeObject(playlistService.Tracks, Formatting.Indented);
+        }
+    }
+}
