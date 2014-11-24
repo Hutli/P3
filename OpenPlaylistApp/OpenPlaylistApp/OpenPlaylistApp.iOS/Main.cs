6d14796c0518af20ecd31a1487a3f406e4da946e
Simon Vandel Sillesen <simon.vandel@gmail.com>
2014-11-24 16:32:38 +0100

    change iOS user id

--- OpenPlaylistApp/OpenPlaylistApp/OpenPlaylistApp.iOS/Main.cs
+++ OpenPlaylistApp/OpenPlaylistApp/OpenPlaylistApp.iOS/Main.cs
@@ -10,7 +10,7 @@ namespace OpenPlaylistApp.iOS
         static void Main(string[] args)
         {
             var uid = UIDevice.CurrentDevice.IdentifierForVendor.AsString();
-            App.User = new User(uid);
+			App.User.Id = uid;
             
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
