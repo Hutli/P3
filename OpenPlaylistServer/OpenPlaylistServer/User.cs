469f56ff3176946b76237b0585447b8a1fce4319
Simon Vandel Sillesen <simon.vandel@gmail.com>
2014-11-25 12:04:36 +0100

    fix update fane not updating on revote

--- OpenPlaylistServer/OpenPlaylistServer/User.cs
+++ OpenPlaylistServer/OpenPlaylistServer/User.cs
@@ -1,11 +1,33 @@
+using System.ComponentModel;
+
 namespace OpenPlaylistServer {
-    public class User {
+    public class User : INotifyPropertyChanged
+    {
+        private PlaylistTrack _vote;
         public string Id { get; private set; }
 
-        public PlaylistTrack Vote { get; set; }
+        public PlaylistTrack Vote
+        {
+            get { return _vote; }
+            set
+            {
+                _vote = value;
+                OnPropertyChanged("Vote");
+            }
+        }
 
         public User(string id) {
             Id = id;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string pName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(pName));
+            }
+        } 
     }
 }
