8899e9a935b2cfed726193d1200f544d5d971a6b
cnHeider <mrnaah@gmail.com>
2014-11-26 10:36:02 +0100

    So nice layout in nowplaying

--- OpenPlaylistApp/OpenPlaylistApp/Views/NowPlayingView.cs
+++ OpenPlaylistApp/OpenPlaylistApp/Views/NowPlayingView.cs
@@ -11,7 +11,6 @@ namespace OpenPlaylistApp.Views
     {
         private Label _nowPlayingLabel = new Label();
         private Label _trackTitleLabel = new Label();
-        private Label _trackArtistLabel = new Label();
         private Image _trackImage = new Image();
         NowPlayingViewModel npvm;
         private RelativeLayout layout = new RelativeLayout();
@@ -25,14 +24,17 @@ namespace OpenPlaylistApp.Views
             _trackImage.Opacity = 0.55f;
             _trackImage.Aspect = Aspect.AspectFill;
 
+            _nowPlayingLabel.Font = Font.BoldSystemFontOfSize(NamedSize.Micro); // Maybe implement side scrolling text if clipped by parent
+            _trackTitleLabel.Font = Font.SystemFontOfSize(NamedSize.Large);
+            _trackTitleLabel.TextColor = Color.White;
+           
             layout.MinimumHeightRequest = 80f;
             layout.IsClippedToBounds = true;
-            layout.HeightRequest = 80f;
+            layout.HeightRequest = 100f;
 
             layout.Children.Add(_trackImage, Constraint.Constant(0), Constraint.Constant(0), Constraint.RelativeToParent((parent) => { return parent.Width; }),Constraint.RelativeToParent((parent) => { return parent.Height; }));
             layout.Children.Add(_nowPlayingLabel, Constraint.Constant(0));
-            layout.Children.Add(_trackTitleLabel, Constraint.Constant(0), Constraint.RelativeToParent((parent) => { return parent.Height / 2; }));
-            layout.Children.Add(_trackArtistLabel, Constraint.RelativeToParent((parent) => { return parent.Width / 2; }),Constraint.RelativeToParent((parent) => { return parent.Height / 2; }));
+            layout.Children.Add(_trackTitleLabel, Constraint.Constant(0), Constraint.RelativeToParent((parent) => { return (parent.Height / 2)-_trackTitleLabel.Height; }));
             layout.Children.Add(progressBar, Constraint.Constant(0), Constraint.RelativeToParent((parent) => { return parent.Height - progressBar.Height; }), Constraint.RelativeToParent((parent) => { return parent.Width; }));
             Content = layout;
         }
@@ -45,10 +47,9 @@ namespace OpenPlaylistApp.Views
 
         void OnLoadComplete()
         {
-            _trackTitleLabel.Text = npvm.Result.Name;
-            _trackArtistLabel.Text = npvm.Result.Album.Artists[0].Name;
+            _trackTitleLabel.Text = npvm.Result.Name + " - " + npvm.Result.Album.Artists[0].Name;
             _trackImage.Source = npvm.Result.Album.Images[0].URL;
-            progressBar.Progress = Convert.ToDouble(npvm.Result.currentDurationStep) / Convert.ToDouble(npvm.Result.Duration)*10;
+            progressBar.Progress = Convert.ToDouble(npvm.Result.currentDurationStep) / Convert.ToDouble(npvm.Result.Duration);
             progressBar.ProgressTo(1, Convert.ToUInt32(npvm.Result.Duration - npvm.Result.currentDurationStep), Easing.Linear);
         }
     }
