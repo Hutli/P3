026feee5fa4a8d2df6d485d16086bf5c2c24f2a6
hutli <jensstaermose@hotmail.com>
2014-11-28 15:08:13 +0100

    Pictures

--- OpenPlaylistApp/OpenPlaylistApp.WinPhone/WebAPI/Track.cs
+++ OpenPlaylistApp/OpenPlaylistApp.WinPhone/WebAPI/Track.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using Newtonsoft.Json;
 
 namespace WebAPI
@@ -18,6 +19,7 @@ namespace WebAPI
             Album = album;
             IsFiltered = false;
         }
+
 
         public Track()
         {
