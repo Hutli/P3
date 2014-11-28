9ba1017ddeb211ec1c15f73e9495793d202ea781
hutli <jensstaermose@hotmail.com>
2014-11-28 12:44:12 +0100

    Unique id to android

--- OpenPlaylistApp/OpenPlaylistApp.iOS/Main.cs
+++ OpenPlaylistApp/OpenPlaylistApp.iOS/Main.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using MonoTouch.UIKit;
 using OpenPlaylistApp.Models;
+using MonoTouch.Security;
+using MonoTouch.Foundation;
 
 namespace OpenPlaylistApp.iOS
 {
@@ -9,13 +11,37 @@ namespace OpenPlaylistApp.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            var uid = UIDevice.CurrentDevice.IdentifierForVendor.AsString();
-			App.User.Id = uid;
-            
+            //var uid = UIDevice.CurrentDevice.IdentifierForVendor.AsString();
+            //App.User.Id = uid;
+
+            App.User.Id = UniqueID();
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
            
+        }
+
+        public static string UniqueID()
+        {
+            var query = new SecRecord(SecKind.GenericPassword);
+            query.Service = NSBundle.MainBundle.BundleIdentifier;
+            query.Account = "UniqueID";
+
+            NSData uniqueId = SecKeyChain.QueryAsData(query);
+            if (uniqueId == null)
+            {
+                query.ValueData = NSData.FromString(System.Guid.NewGuid().ToString());
+                var err = SecKeyChain.Add(query);
+                if (err != SecStatusCode.Success && err != SecStatusCode.DuplicateItem)
+                    throw new Exception("Cannot store Unique ID");
+
+                return query.ValueData.ToString();
+            }
+            else
+            {
+                return uniqueId.ToString();
+            }
         }
     }
 }
