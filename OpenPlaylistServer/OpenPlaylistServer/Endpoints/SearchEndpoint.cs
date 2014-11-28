74491e5fea801c6194325e7d61904247c6c0de0d
hutli <jensstaermose@hotmail.com>
2014-11-28 14:30:18 +0100

    Playlist en search hack

--- OpenPlaylistServer/OpenPlaylistServer/Endpoints/SearchEndpoint.cs
+++ OpenPlaylistServer/OpenPlaylistServer/Endpoints/SearchEndpoint.cs
@@ -4,6 +4,7 @@ using Nancy;
 using Newtonsoft.Json;
 using OpenPlaylistServer.Models;
 using OpenPlaylistServer.Services.Interfaces;
+using WebAPI;
 
 namespace OpenPlaylistServer.Endpoints
 {
@@ -15,12 +16,20 @@ namespace OpenPlaylistServer.Endpoints
             new Restriction(track => track.Name != "Still Alive")
         };
 
-        public SearchEndpoint(ISearchService searchService, IFilterService filterService)
+        public SearchEndpoint(ISearchService searchService, IFilterService filterService, IPlaylistService playlistService)
         {
             Get["/search/{query}"] = parameters =>
             {
                 var tracks = searchService.Search(parameters.query);
                 filterService.FilterTracks(tracks, restrictions);
+                foreach (Track t in tracks)
+                {
+                    Track tmpTrack = playlistService.Tracks.FirstOrDefault(p => p.Equals(t));
+                    if (tmpTrack != null)
+                    {
+                        t.TScore = tmpTrack.TotalScore;
+                    }
+                }
                 return JsonConvert.SerializeObject(tracks, Formatting.Indented);
             };
         }
