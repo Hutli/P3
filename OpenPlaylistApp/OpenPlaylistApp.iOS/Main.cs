bdddee58da93e6d913d48b6ca6f3b9032dc42613
hutli <jensstaermose@hotmail.com>
2014-11-28 13:08:46 +0100

    Wrong platform code

--- OpenPlaylistApp/OpenPlaylistApp.iOS/Main.cs
+++ OpenPlaylistApp/OpenPlaylistApp.iOS/Main.cs
@@ -11,37 +11,13 @@ namespace OpenPlaylistApp.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            //var uid = UIDevice.CurrentDevice.IdentifierForVendor.AsString();
-            //App.User.Id = uid;
-
-            App.User.Id = UniqueID();
+            var uid = UIDevice.CurrentDevice.IdentifierForVendor.AsString();
+            App.User.Id = uid;;
 
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
            
-        }
-
-        public static string UniqueID()
-        {
-            var query = new SecRecord(SecKind.GenericPassword);
-            query.Service = NSBundle.MainBundle.BundleIdentifier;
-            query.Account = "UniqueID";
-
-            NSData uniqueId = SecKeyChain.QueryAsData(query);
-            if (uniqueId == null)
-            {
-                query.ValueData = NSData.FromString(System.Guid.NewGuid().ToString());
-                var err = SecKeyChain.Add(query);
-                if (err != SecStatusCode.Success && err != SecStatusCode.DuplicateItem)
-                    throw new Exception("Cannot store Unique ID");
-
-                return query.ValueData.ToString();
-            }
-            else
-            {
-                return uniqueId.ToString();
-            }
         }
     }
 }
