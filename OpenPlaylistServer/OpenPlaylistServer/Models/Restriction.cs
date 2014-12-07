4a6f1456769b30f4f051e48aa096cb3f3b312733
cnHeider <mrnaah@gmail.com>
2014-12-07 19:00:41 +0100

    Small fixes, logic still flawed

--- OpenPlaylistServer/OpenPlaylistServer/Models/Restriction.cs
+++ OpenPlaylistServer/OpenPlaylistServer/Models/Restriction.cs
@@ -37,7 +37,7 @@ namespace OpenPlaylistServer.Models
                     .Concat(artistsParsed) // append the artists parsed to the existing restrictionunits
                     .ToArray();
 
-                UpdatePredicate(RestrictionType, _restrictionUnits);
+                Predicate = UpdatePredicate(RestrictionType, _restrictionUnits);
 
                 OnPropertyChanged("Artists");
 
@@ -71,7 +71,7 @@ namespace OpenPlaylistServer.Models
                     .Concat(titlesParsed) // append the artists parsed to the existing restrictionunits
                     .ToArray();
 
-                UpdatePredicate(RestrictionType, _restrictionUnits);
+                Predicate = UpdatePredicate(RestrictionType, _restrictionUnits);
 
                 OnPropertyChanged("Titles");
             }
@@ -80,7 +80,7 @@ namespace OpenPlaylistServer.Models
         public RestrictionType RestrictionType
         {
             get { return _restrictionType; }
-            set
+            private set
             {
                 _restrictionType = value;
                 OnPropertyChanged("RestrictionType");
@@ -88,12 +88,9 @@ namespace OpenPlaylistServer.Models
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        private Func<Track,bool> _predicate;
 
         private RestrictionType _restrictionType;
         private RestrictionUnit[] _restrictionUnits;
-        private string _name;
-        private string _artists;
         private DateTime _startTime;
         private DateTime _endTime;
 
@@ -138,7 +135,7 @@ namespace OpenPlaylistServer.Models
         {
             var filter = UpdatePredicate(restrictionType, restrictionUnits);
 
-            _predicate = filter;
+            Predicate = filter;
             _startTime = timeStart;
             _endTime = timeEnd;
             _restrictionType = restrictionType;
@@ -151,6 +148,8 @@ namespace OpenPlaylistServer.Models
             // chain all restrictionunits together
             foreach (var restrictionUnit in restrictionUnits)
             {
+                if(string.IsNullOrWhiteSpace(restrictionUnit.FieldValue))
+                    continue;
                 var prevFilter = filter;
                 RestrictionUnit unit = restrictionUnit;
                 Func<Track, bool> currentFilter = t =>
@@ -167,22 +166,19 @@ namespace OpenPlaylistServer.Models
                     }
                 };
 
-                if (restrictionType == RestrictionType.BlackList)
-                {
-                    Func<Track, bool> filter1 = currentFilter;
-                    currentFilter = t => !filter1(t);
-                }
-
                 filter = t => prevFilter(t) && currentFilter(t);
             }
+
+            if (restrictionType == RestrictionType.BlackList)
+            {
+                Func<Track, bool> filter1 = filter;
+                filter = t => !filter1(t);
+            }
+
             return filter;
         }
 
-        public Func<Track, bool> Predicate
-        {
-            get { return _predicate; }
-            private set { _predicate = value; }
-        }
+        public Func<Track, bool> Predicate { get; private set; }
 
         //ToStringPredicate
 
