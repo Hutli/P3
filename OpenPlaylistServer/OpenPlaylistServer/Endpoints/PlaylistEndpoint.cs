e6c8725e39962ecd70242277051a4ecae4da9cee
Simon Vandel Sillesen <simon.vandel@gmail.com>
2014-12-12 15:55:25 +0100

    sort playlist using totalscore

--- OpenPlaylistServer/OpenPlaylistServer/Endpoints/PlaylistEndpoint.cs
+++ OpenPlaylistServer/OpenPlaylistServer/Endpoints/PlaylistEndpoint.cs
@@ -11,7 +11,7 @@ namespace OpenPlaylistServer.Endpoints
         public PlaylistEndpoint(IPlaylistService playlistService)
         {
             var tracks = playlistService.Tracks;
-            var ordered = tracks.OrderByDescending(t => t.TScore).Where(t => t.TotalScore != 0).Select((t, index) => {
+            var ordered = tracks.OrderByDescending(t => t.TotalScore).Where(t => t.TotalScore != 0).Select((t, index) => {
                 t.Rank = index + 1;
                 return t;
             });
